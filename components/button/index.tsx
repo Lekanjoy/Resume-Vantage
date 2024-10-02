192ee661fb18f14ab1518ba174f6fb6e6600acdb
Lekanjoy <alabilekanemmanuel@gmail.com>
2024-10-02 11:55:18 +0100

    "Updated Button component to use 'use client' and conditional href based on auth status"

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Link from "next/link";
+import { useAuthStatus } from "@/hooks/useAuthStatus";
 import ArrowIcon from "../icon/ArrowIcon";
 
 type ButtonProps = {
@@ -8,17 +10,23 @@ type ButtonProps = {
   stroke: string;
 } & React.InputHTMLAttributes<HTMLAnchorElement>;
 
-const Button = ({ text, bgColor, textColor, stroke }: ButtonProps) => (
-  <Link
-    href="auth/sign-up"
-    className={`whitespace-nowrap w-fit flex items-center gap-x-2 py-3 px-5 ${bgColor} rounded-md ${textColor} ease-in-out duration-300 lg:hover:opacity-80`}>
-    <span
-      className={`text-xs font-semibold lg:text-base sm:text-sm 2xl:text-lg`}
+const Button = ({ text, bgColor, textColor, stroke }: ButtonProps) => {
+  const { isLoggedIn } = useAuthStatus();
+  const currentHref = isLoggedIn ? "/dashboard" : "/auth/sign-up";
+
+  return (
+    <Link
+      href={currentHref}
+      className={`whitespace-nowrap w-fit flex items-center gap-x-2 py-3 px-5 ${bgColor} rounded-md ${textColor} ease-in-out duration-300 lg:hover:opacity-80`}
     >
-      {text}
-    </span>
-    <ArrowIcon stroke={stroke} />
-  </Link>
-);
+      <span
+        className={`text-xs font-semibold lg:text-base sm:text-sm 2xl:text-lg`}
+      >
+        {text}
+      </span>
+      <ArrowIcon stroke={stroke} />
+    </Link>
+  );
+};
 
 export default Button;
